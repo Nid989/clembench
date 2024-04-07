dc7c1d7a783c61421a99417d744f6de9dd59380d
nid989 <nidbhavsar989@gmail.com>
2024-04-07 17:34:23 +0200

    added `imagegame` dataformatter to manual_analysis

--- analysis/manual_analysis.py
+++ analysis/manual_analysis.py
@@ -12,6 +12,7 @@ from analysis.utils import load_from_json, merge_dfs_on_columns, format_cllm_pai
  
 # Extract the initial turn-wise interactions b/w game-master (gm) and player 1 (player1) for Taboo game
 class taboogame_init_data_formatter:
+    """Testing for World Knowledge"""
     def __init__(self, cllm_name: str, level: str="1_medium_en", with_cot: bool=False):
         self.clemgame = "taboo" if not with_cot else "taboo_cot"
         self.cllm_name = cllm_name
@@ -22,8 +23,6 @@ class taboogame_init_data_formatter:
     def _extract_init_ep_interactions_player1_all(self):
         all_init_interactions_player1 = collections.defaultdict(list)
         # sorted episodes based on the cardinality and not `string` type
-        print(os.path.join(self.path_to_model_dir, f"./{self.clemgame}/{self.level}/"))
-        print(os.path.exists(os.path.join(self.path_to_model_dir, f"./{self.clemgame}/{self.level}/")))
         episodes = [filename for filename in os.listdir(os.path.join(self.path_to_model_dir, f"./{self.clemgame}/{self.level}/")) if filename.startswith("episode")]
         episodes = sorted(episodes, key=lambda x: int(x.split("_")[-1]))
         for index, episode in enumerate(episodes):
@@ -61,6 +60,7 @@ class taboogame_init_data_formatter:
     
 # Extract the turn-wise interactions b/w game-master (gm) and player 1 (player1) for Wordle game (strictly `withclue`)
 class wordle_withclue_data_formatter:
+    """Testing for World Knowledge (initial utterance), Situation Modeling (utterances over turns)"""
     def __init__(self, cllm_name: str, level: str="1_medium_frequency_words_clue_no_critic", with_cot: bool=False):
         self.clemgame = "wordle_withclue" if not with_cot else "wordle_withclue_cot"
         self.cllm_name = cllm_name
@@ -136,6 +136,7 @@ class wordle_withclue_data_formatter:
 
 # Extract the interactions b/w game-master (gm) and player 1 (player1) for Reference game
 class referencegame_data_formatter:
+    """Testing for World Knowledge"""
     def __init__(self, cllm_name: str, level: str="0_line_grids_rows", with_cot: bool=False):
         self.clemgame = "referencegame" if not with_cot else "referencegame_cot"
         self.cllm_name = cllm_name
@@ -182,6 +183,66 @@ class referencegame_data_formatter:
             f"player1_response_{self.cllm_name}": player1_response
         }
 
+class imagegame_data_formatter:
+    """Testing for Situation Modeling"""
+    def __init__(self, cllm_name: str, level: str="0_line_grids_rows", with_cot: bool=False):
+        self.clemgame = "imagegame" if not with_cot else "imagegame_cot"
+        self.cllm_name = cllm_name
+        self.level = level
+        self.path_to_model_dir = os.path.join(path_to_results_dir, format_cllm_pair(self.cllm_name))
+
+        # extract the utterance b/w game-master (gm) and player 1 (specifiying the grids to `player_1` and corresponding response.)
+        self._extract_ep_interactions_player2_all()
+
+    def _extract_ep_interactions_player2_all(self):
+        all_interactions_player2 = collections.defaultdict(list)
+        # sorted episodes based on the cardinality and not `string` type
+        episodes = [filename for filename in os.listdir(os.path.join(self.path_to_model_dir, f"./{self.clemgame}/{self.level}/")) if filename.startswith("episode")]
+        episodes = sorted(episodes, key=lambda x: int(x.split("_")[-1]))
+        for index, episode in enumerate(episodes):
+            # load interactions.json file (incl. turn-wise interactions b/w gm, player1, player2)
+            path_to_ep_interactions_file = os.path.join(self.path_to_model_dir, f"./{self.clemgame}/{self.level}/{episode}/interactions.json")
+            path_to_ep_instance_file = os.path.join(self.path_to_model_dir, f"./{self.clemgame}/{self.level}/{episode}/instance.json")
+            
+            if os.path.exists(path_to_ep_interactions_file):
+                interactions_data = load_from_json(path_to_ep_interactions_file)
+                instance_data = load_from_json(path_to_ep_instance_file)
+                interactions_player2 = self._extract_ep_interactions_player2(interactions_data, instance_data)
+                _ = [all_interactions_player2[key].append(value) for key, value in interactions_player2.items()]
+        self.all_interactions_player2_df = pd.DataFrame(all_interactions_player2)
+        self.all_interactions_player2_df[f"score_{self.cllm_name}"] = ''
+        
+    def _extract_ep_interactions_player2(self, ep_interactions_data: Dict, ep_instance_data: Dict):
+        def extract_w_key_value_pair(turns_data: List[Dict[str, str]], key_value_pair: Tuple[str]):
+            return next((index, item) for index, item in enumerate(turns_data) if item.get(key_value_pair[0]) == key_value_pair[1])
+        target_grid = ep_instance_data["target_grid"]
+        ep_utterances_processed = []
+        total_turns = len(ep_interactions_data["turns"])
+        for turn_id in range(total_turns):
+            ep_utterances_processed.append(f"### turn: {turn_id} ###")
+            if turn_id == total_turns-1: # TODO: cross-check if final-turns usually have gm->player1 utterances
+                continue
+            if turn_id == 0:
+                gm_to_player2 = extract_w_key_value_pair(ep_interactions_data["turns"][turn_id], ("to", "Player 2"))
+                _, player2_to_gm = extract_w_key_value_pair(ep_interactions_data["turns"][turn_id], ("from", "Player 2"))
+                # extract the first drawing instruction given by gm (game-master) to player2 
+                gm_instruction = gm_to_player2[-1]["action"]["content"].split(".")[-1].strip() 
+                player2_response = player2_to_gm["action"]["content"].strip()
+                ep_utterances_processed.append(f"## GM instruction ##\n{gm_instruction}")
+                ep_utterances_processed.append(f"## Player 2 response ##\n{player2_response}")
+            else:
+                _, gm_to_player2 = extract_w_key_value_pair(ep_interactions_data["turns"][turn_id], ("to", "Player 2"))
+                _, player2_to_gm = extract_w_key_value_pair(ep_interactions_data["turns"][turn_id], ("from", "Player 2"))
+                # extract the first drawing instruction given by gm (game-master) to player2 
+                gm_instruction = gm_to_player2["action"]["content"].strip() 
+                player2_response = player2_to_gm["action"]["content"].strip()
+                ep_utterances_processed.append(f"## GM instruction ##\n{gm_instruction}")
+                ep_utterances_processed.append(f"## Player 2 response ##\n{player2_response}")
+        return {
+            "prompt_instruction": target_grid,
+            f"player2_gm_utterances_{self.cllm_name}": "\n".join(ep_utterances_processed),
+        }
+    
 class clembench_emergence_annotation_extractor:
     def __init__(self, clemgame: str="taboo", level: str="0_high_en", models: List[str]=None, experiment_name: str=None, with_cot: bool=False):
         self.clemgame = clemgame
@@ -193,7 +254,7 @@ class clembench_emergence_annotation_extractor:
 
     def _setup(self):
         # check for value errors for input attributes
-        if self.clemgame not in ["taboo", "wordle_withclue", "referencegame"]:
+        if self.clemgame not in ["taboo", "wordle_withclue", "referencegame", "imagegame"]:
             raise ValueError("The input attribute clemgame should be one of ['taboo', 'wordle_withclue', 'referencegame'], found: {}".format(self.clemgame))
         if self.level not in game_level_types[self.clemgame]:
             raise ValueError("The input attribute level should be one of {}, found: {}".format(game_level_types[self.clemgame], self.level))
@@ -202,11 +263,13 @@ class clembench_emergence_annotation_extractor:
             "taboo": taboogame_init_data_formatter,
             "wordle_withclue": wordle_withclue_data_formatter,
             "referencegame": referencegame_data_formatter,
+            "imagegame": imagegame_data_formatter 
         }
         manual_analysis_data_containers = {
             "taboo": "all_init_interactions_player1_df",
             "wordle_withclue": "all_interactions_player1_df",
-            "referencegame": "all_interactions_player1_df"
+            "referencegame": "all_interactions_player1_df",
+            "imagegame": "all_interactions_player2_df"
         }
 
         self.all_models_data_formatters = [manual_analysis_data_formatters[self.clemgame](cllm_name=model_name,
@@ -222,8 +285,8 @@ class clembench_emergence_annotation_extractor:
         save_to_excel(self.extracted_annotations, os.path.join(path_to_outputs_dir, self.filename))
         
 if __name__ == "__main__":
-    out = clembench_emergence_annotation_extractor(clemgame="referencegame", 
-                                                   level="0_line_grids_rows", 
+    out = clembench_emergence_annotation_extractor(clemgame="imagegame", 
+                                                   level="0_compact_grids", 
                                                    models=["fsc-openchat-3.5-0106"], 
                                                    experiment_name="test_experiment", 
                                                    with_cot=True)
